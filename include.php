b6979996548e656e2ad2ec166881a6241efa2653
Ashraf5555 <529840@edu.rocmn.nl>
2022-11-17 13:55:14 +0100

    simple-search

---
+++ include.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Filename:include.php
+ * Date: 1-06-2021
+ * Author: Ashraf Soudah
+ */
+    
+    define("DB_SERVER", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_NAME", "tutorials");
+	
+?>
